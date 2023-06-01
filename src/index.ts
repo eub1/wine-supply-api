67900cf89e45c89c313ec0b35b268164580d747a
eub1 <ebonforte@gmail.com>
2023-06-01 20:05:35 -0300

    add to port: || 3001;

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import { config } from "dotenv";
 config();
 import mongoose from "mongoose";
-import { app } from './App'
+import { app } from "./App";
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001;
 
-mongoose.connect(process.env.MONGO_URL!).then(()=>{
-  console.log(`Listening on port ${PORT}`);
-  app.listen(PORT);
-}).catch((error) => {'connection failed due to error: ' + error.message})
+mongoose
+	.connect(process.env.MONGO_URL!)
+	.then(() => {
+		console.log(`Listening on port ${PORT}`);
+		app.listen(PORT);
+	})
+	.catch((error) => {
+		"connection failed due to error: " + error.message;
+	});
 
-const db = mongoose.connection
-db.on("error", console.error.bind(console, "mongoDB connection error"))
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongoDB connection error"));
